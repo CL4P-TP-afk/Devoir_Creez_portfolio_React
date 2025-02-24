86fb1d54b428de50e09c33b805227080099749d6
loicfleury <loic.fleury28@gmail.com>
2025-02-24 17:07:21 +0100

    texte bg et menu burger color

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -4,28 +4,28 @@ const Nav = () => {
     return (
         <>
             <header>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+            <nav class="navbar navbar-expand-lg bg-dark"  data-bs-theme="dark">
                 <div class="container-fluid ">
-                    <span class="navbar-brand mb-0 h1" >John Doe</span>
+                    <span class="navbar-brand mb-0 h1 text-white" >John Doe</span>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon "></span>
                     </button>                
                     <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
                         <ul class="navbar-nav mb-2 mb-lg-0 ">
                             <li class="nav-item">
-                                <Link to="/" class="nav-link" >Home</Link>
+                                <Link to="/" class="nav-link text-white" >Home</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="/services" class="nav-link" >services</Link>
+                                <Link to="/services" class="nav-link text-white" >services</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="/portfolio" class="nav-link">portfolio</Link>
+                                <Link to="/portfolio" class="nav-link text-white">portfolio</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="/contact" class="nav-link" >contact</Link>    
+                                <Link to="/contact" class="nav-link text-white" >contact</Link>    
                             </li>
                             <li class="nav-item">
-                                <Link to="/mentionslegales" class="nav-link" >Mentions Légales</Link>
+                                <Link to="/mentionslegales" class="nav-link text-white" >Mentions Légales</Link>
                             </li>
                         </ul>
                     </div>
