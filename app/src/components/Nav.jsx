24644c004b31b2b9db1656d25a6cf314ee6a0a6e
loicfleury <loic.fleury28@gmail.com>
2025-02-24 15:31:01 +0100

    positionnement des liens du nav

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -4,28 +4,28 @@ const Nav = () => {
     return (
         <>
             <header>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+                <div class="container-fluid ">
                     <span class="navbar-brand mb-0 h1" >John Doe</span>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>                
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+                        <ul class="navbar-nav mb-2 mb-lg-0 ">
                             <li class="nav-item">
-                                <Link to="/" class="nav-link active" aria-current="page">Home</Link>
+                                <Link to="/" class="nav-link" >Home</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="/services" class="nav-link active" aria-current="page">services</Link>
+                                <Link to="/services" class="nav-link" >services</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="/portfolio" class="nav-link active" aria-current="page">portfolio</Link>
+                                <Link to="/portfolio" class="nav-link">portfolio</Link>
                             </li>
                             <li class="nav-item">
-                                <Link to="/contact" class="nav-link active" aria-current="page">contact</Link>    
+                                <Link to="/contact" class="nav-link" >contact</Link>    
                             </li>
                             <li class="nav-item">
-                                <Link to="/mentionslegales" class="nav-link active" aria-current="page">Mentions Légales</Link>
+                                <Link to="/mentionslegales" class="nav-link" >Mentions Légales</Link>
                             </li>
                         </ul>
                     </div>
