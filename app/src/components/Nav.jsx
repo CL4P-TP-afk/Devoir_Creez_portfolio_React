5c8a2f063d0badd4cede2e0db07739bc3124621e
loicfleury <loic.fleury28@gmail.com>
2025-02-26 11:34:42 +0100

    utilisation des props react pour les liens

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,12 +1,33 @@
 import { NavLink } from "react-router";
 
+const Lien = (props) => {
+  return (
+    <li class="nav-item">
+      <NavLink
+        to={props.page}
+        style={({ isActive }) => {
+          return {
+            fontWeight: isActive ? "bold" : "",
+            color: isActive ? "red" : "",
+            textDecoration: isActive ? "underline" : "",
+          };
+        }}
+      >
+        {props.lien}
+      </NavLink>
+    </li>
+  );
+};
+
 const Nav = () => {
   return (
     <>
       <header>
         <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
           <div class="container-fluid ">
-            <span class="navbar-brand mb-0 h1 text-white">John Doe</span>
+            <span class="navbar-brand mb-0 h1 text-white text-uppercase">
+              John Doe
+            </span>
             <button
               class="navbar-toggler"
               type="button"
@@ -23,76 +44,11 @@ const Nav = () => {
               id="navbarSupportedContent"
             >
               <ul class="navbar-nav mb-2 mb-lg-0 ">
-                <li class="nav-item">
-                  <NavLink
-                    to="/home"
-                    style={({ isActive }) => {
-                      return {
-                        fontWeight: isActive ? "bold" : "",
-                        color: isActive ? "red" : "",
-                        textDecoration: isActive ? "underline" : "",
-                      };
-                    }}
-                  >
-                    Home
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink
-                    to="/services"
-                    style={({ isActive }) => {
-                      return {
-                        fontWeight: isActive ? "bold" : "",
-                        color: isActive ? "red" : "",
-                        textDecoration: isActive ? "underline" : "",
-                      };
-                    }}
-                  >
-                    Services
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink
-                    to="/portfolio"
-                    style={({ isActive }) => {
-                      return {
-                        fontWeight: isActive ? "bold" : "",
-                        color: isActive ? "red" : "",
-                        textDecoration: isActive ? "underline" : "",
-                      };
-                    }}
-                  >
-                    Portofolio
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink
-                    to="/contact"
-                    style={({ isActive }) => {
-                      return {
-                        fontWeight: isActive ? "bold" : "",
-                        color: isActive ? "red" : "",
-                        textDecoration: isActive ? "underline" : "",
-                      };
-                    }}
-                  >
-                    Contact
-                  </NavLink>
-                </li>
-                <li class="nav-item">
-                  <NavLink
-                    to="/mentionslegales"
-                    style={({ isActive }) => {
-                      return {
-                        fontWeight: isActive ? "bold" : "",
-                        color: isActive ? "red" : "",
-                        textDecoration: isActive ? "underline" : "",
-                      };
-                    }}
-                  >
-                    Mentions Légales
-                  </NavLink>
-                </li>
+                <Lien lien="Home" page="/" />
+                <Lien lien="Services" page="/services" />
+                <Lien lien="Portofolio" page="/portfolio" />
+                <Lien lien="Contact" page="/contact" />
+                <Lien lien="Mentions Légales" page="/mentionslegales" />
               </ul>
             </div>
           </div>
