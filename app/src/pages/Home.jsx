dc3b2acae28d5368208410159f5da75eccdd9174
loicfleury <loic.fleury28@gmail.com>
2025-03-04 18:10:49 +0100

    bg hero en cour

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import "./home.css";
+
+
+
 
 const Home = () => {
     return (
         <>
-            <h1>Page d'acceuil</h1>
+
+        <h1 >Page d'acceuil</h1>
+        <img src="../src/assets/images/hero-bg.jpg" className="img-fluid"></img>
+            
+        
         </>
     )
 }
