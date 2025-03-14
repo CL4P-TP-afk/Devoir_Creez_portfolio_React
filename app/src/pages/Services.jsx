d5421c76d325e98a57c0451b95f6de91124049a9
loicfleury <loic.fleury28@gmail.com>
2025-03-14 17:33:30 +0100

    debut du placement des elements

--- app/src/pages/Services.jsx
+++ app/src/pages/Services.jsx
@@ -3,6 +3,40 @@ const Services = () => {
     return (
         <>
             <h1>Page Services</h1>
+            <img src="./src/assets/images/banner.jpg" alt="ligne lumineuse droite puis partant dans tous les sens sur fond bleu foncé" class="img-fluid" />
+            <div className="text-center border-bottom border-primary border-4 w-25 justify-content-center">
+                <h2><strong>Mon offre de services</strong></h2>
+                <p>voici les prestations sur lesquelles je peux intervenir</p>
+            </div>
+            <div class="row">
+                <div class="col-md-4 ">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
