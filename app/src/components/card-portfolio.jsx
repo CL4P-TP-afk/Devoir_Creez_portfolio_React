4958f0ba433c736c3adbb5cc4b561772dcdae320
loicfleury <loic.fleury28@gmail.com>
2025-03-25 15:02:44 +0100

    effects fonctionnels , fin page portfolio , end commit

--- app/src/components/card-portfolio.jsx
+++ app/src/components/card-portfolio.jsx
@@ -4,24 +4,24 @@ import "../pages/card-effect.css";
 const CardPortfolio = (props) => {
     return (
         
-        <Card style={{width: '100%'}} className='h-100 card-hover'>
-            <CardHeader className='p-0'>
-                <CardImg alt={props.alt} src={props.src} width="100%" />
-            </CardHeader>                  
-            <CardBody >
-                <CardTitle tag="h2">
-                {props.title}
-                </CardTitle>
-                <CardText>
-                {props.text}
-                </CardText>
-                <Button href="#" tag="a" color="primary">Voir le site</Button>
-            </CardBody>
-            <CardFooter>
-            {props.footer}
-            </CardFooter>
-        </Card>
-        
+            <Card style={{width: '100%'}} className='h-100 card-hover'>
+                <CardHeader className='p-0'>
+                    <CardImg alt={props.alt} src={props.src} width="100%" />
+                </CardHeader>                  
+                <CardBody >
+                    <CardTitle tag="h2">
+                    {props.title}
+                    </CardTitle>
+                    <CardText>
+                    {props.text}
+                    </CardText>
+                    <Button href="#" tag="a" color="primary">Voir le site</Button>
+                </CardBody>
+                <CardFooter>
+                {props.footer}
+                </CardFooter>
+            </Card>
+       
     );
 };
 
