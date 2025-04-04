9629f3d766929a6bbdca6c1a9fb9805c1c4601d1
loicfleury <loic.fleury28@gmail.com>
2025-04-04 16:57:20 +0200

    creation d'un composant adressJD , mise en place de l accordion Mentions légales

--- app/src/pages/Mentions-legales.jsx
+++ app/src/pages/Mentions-legales.jsx
@@ -1,10 +1,69 @@
+import { Container,  Row, Accordion, AccordionBody, AccordionHeader, AccordionItem } from "reactstrap";
+import React, { useState } from 'react';
 
-const MentionsLegales = () => {
+
+
+const MentionsLegales = (props) => {
+    const [open, setOpen] = useState('1');
+    const toggle = (id) => {
+            if (open === id) {
+            setOpen();
+            } else {
+            setOpen(id);
+            }
+        };   
+    
     return (
         <>
-            <h1>Page Mentions Légales</h1>
+        <Container className=" h-100 py-4">
+            <Row className="justify-content-center py-3 text-center">
+            <h1>Mentions légales</h1>
+            <hr
+                className="border border-primary border-3 opacity-100 my-4 "
+                style={{ width: "40%" }}
+            />
+            </Row>
+        
+            <Row>
+            <Accordion open={open} toggle={toggle}>
+            <AccordionItem>
+            <AccordionHeader targetId="1">Accordion Item 1</AccordionHeader>
+            <AccordionBody accordionId="1">
+                <strong>This is the first item&#39;s accordion body.</strong>
+                You can modify any of this with custom CSS or overriding our default
+                variables. It&#39;s also worth noting that just about any HTML can
+                go within the <code>.accordion-body</code>, though the transition
+                does limit overflow.
+            </AccordionBody>
+            </AccordionItem>
+            <AccordionItem>
+            <AccordionHeader targetId="2">Accordion Item 2</AccordionHeader>
+            <AccordionBody accordionId="2">
+                <strong>This is the second item&#39;s accordion body.</strong>
+                You can modify any of this with custom CSS or overriding our default
+                variables. It&#39;s also worth noting that just about any HTML can
+                go within the <code>.accordion-body</code>, though the transition
+                does limit overflow.
+            </AccordionBody>
+            </AccordionItem>
+            <AccordionItem>
+            <AccordionHeader targetId="3">Accordion Item 3</AccordionHeader>
+            <AccordionBody accordionId="3">
+                <strong>This is the third item&#39;s accordion body.</strong>
+                You can modify any of this with custom CSS or overriding our default
+                variables. It&#39;s also worth noting that just about any HTML can
+                go within the <code>.accordion-body</code>, though the transition
+                does limit overflow.
+            </AccordionBody>
+            </AccordionItem>
+        </Accordion>
+        </Row>
+      </Container>
         </>
     )
 }
 
 export default MentionsLegales;
+
+
+
